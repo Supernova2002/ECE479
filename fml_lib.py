2f73243cc355f7c0586aaf88c9a4389beb14d29b
David Stekol <dstekol@gmail.com>
2024-04-05 03:06:02 -0400

    fixed issue with reindexing to close in very jank manner

--- fml_lib.py
+++ fml_lib.py
@@ -607,3 +607,17 @@ def get_co_events(close,events):
     return overlap_df
 
 
+def getTimeDecay(tW,clfLastW=1.):
+    # apply piecewise-linear decay to observed uniqueness (tW)
+    # newest observation gets weight=1, oldest observation gets weight=clfLastW
+    clfW=tW.sort_index().cumsum()
+    if clfLastW>=0:
+        slope=(1.-clfLastW)/clfW.iloc[-1]
+    else:
+        slope=1./((clfLastW+1)*clfW.iloc[-1])
+    const=1.-slope*clfW.iloc[-1]
+    clfW=const+slope*clfW
+    clfW[clfW<0]=0
+    return clfW
+
+
